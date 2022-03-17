c30112e3aacee1e47cf404d0d13bcb2e355d32d9
Evan Steiner <100085429+Seeyousooner@users.noreply.github.com>
2022-03-17 13:35:00 -0600

    commit

---
+++ exercises/.idea/exercise7.py
@@ -0,0 +1,22 @@
+# Looping
+# 4-3
+for numbers in range(1, 21):
+    print(numbers)
+
+# 4-6
+odd = []
+odd = list(range(1, 21, 2))
+for number in odd\
+        :
+    print(number)
+
+# 4-7
+multiple_three = list(range(3, 31, 3))
+for number in multiple_three:
+    print(number)
+
+# 4-8
+third_power = []
+for integers in range(1, 11):
+    third_power.append(integers**3)
+print(third_power)
