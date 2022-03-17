7884881c84551e5f8611242930940929fd4a1d29
Evan Steiner <100085429+Seeyousooner@users.noreply.github.com>
2022-03-17 01:57:57 -0600

    commit

---
+++ exercises/.idea/exercise3.py
@@ -0,0 +1,33 @@
+# Strings
+
+# 2-3
+
+first_name = 'Evan'
+print(f" Hi, {first_name.title()}! Good day sir!")
+
+# 2-4
+# lowercase, uppercase, and titlecase
+
+name = 'Evan'
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+# 2-5
+# include quotation marks
+quote_name= 'Albert Einstein'
+print(f'{quote_name.title()} said, "Creativity is intelligence having fun."')
+
+# 2-6
+famous_person = 'Albert Einstein'
+message = f'{famous_person} said, "Creativity is intelligence having fun."'
+print(message)
+
+# 2-7
+name_first = '\tEvan\n'
+print(name_first)
+
+# 1strip(), rstrip(), and strip()
+print(name_first.rstrip())
+print(name_first.strip())
+print(name_first.lstrip())
